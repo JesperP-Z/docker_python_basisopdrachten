2caf1f0d7af23ab216339c5bce919dfee1f30a1f
JesperP-Z <97099105@st.deltion.nl>
2025-09-26 09:52:12 +0200

    Gitpush

--- app/030_berekeningen/opdr_1/opdr_1.py
+++ app/030_berekeningen/opdr_1/opdr_1.py
@@ -1,5 +1,13 @@
 # Opdracht 1 berekeningen
-# Naam student:
-# Groep:
+# Naam student: Jesper Pot 
+# Groep: ITFLEX
 
-# Hier komt je code...
+
+Dennis = 25
+Mark = 40
+Jesper = 100
+
+print("De totale waarden van alle leeftijden bij elkaar opgeteld is: " + str(Dennis + Mark + Jesper))
+print("Het gemiddelde van alle leeftijden is: " + str((Dennis + Mark + Jesper) / 3))
+
+exit()
