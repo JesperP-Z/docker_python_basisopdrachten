50353fa74d6dcd723e88827473acc82a3f314e2b
JesperP-Z <97099105@st.deltion.nl>
2025-10-09 10:57:04 +0200

    modified:   app/060_loops/opdr_5/opdr_5.py 	modified:   app/060_loops/opdr_6/opdr_6.py

--- app/060_loops/opdr_5/opdr_5.py
+++ app/060_loops/opdr_5/opdr_5.py
@@ -1,9 +1,15 @@
 # Opdracht 3 input functie
-# Naam student:
-# Groep:
+# Naam student: Jesper Pot
+# Groep: ITFLEX 
 
 # Hier komt je code...
 
 # Hier start de for-loop
 
 my_list = []
+
+for i in range(3, 20, 3):
+    my_list.append(i)   
+
+print(my_list)
+exit()
--- app/060_loops/opdr_6/opdr_6.py
+++ app/060_loops/opdr_6/opdr_6.py
@@ -1,9 +1,19 @@
 # Opdracht 3 input functie
-# Naam student:
-# Groep:
+# Naam student: Jesper Pot 
+# Groep: ITFLEX 
 
 # Hier komt je code...
 
 # Hier start de for-loop
 
-my_list = []
+my_list = ['margharita', 'calzone', 'verdi', 'olivio', 'quattro stagioni']
+
+
+my_list.append('Americana')
+my_list.remove('olivio')
+my_list.sort()
+
+
+
+print(my_list[-3:])
+exit()
